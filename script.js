dc034a2249465cf86734388d994cb0fbe54c4cc1
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-11-08 17:56:21 -0300

    Feat: creating function

---
+++ script.js
@@ -0,0 +1,14 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
